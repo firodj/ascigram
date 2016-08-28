894cc71675fd790bd3da5c86e8e119689385fa37
Fadhil Mandaga <firodj@gmail.com>
2016-08-28 16:10:58 +0700

    add buffer.c

---
+++ src/buffer.c
@@ -0,0 +1,310 @@
+/* buffer.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "buffer.h"
+
+void *
+ascigram_malloc(size_t size)
+{
+	void *ret = malloc(size);
+
+	if (!ret) {
+		fprintf(stderr, "Allocation failed.\n");
+		abort();
+	}
+
+	return ret;
+}
+
+void *
+ascigram_calloc(size_t nmemb, size_t size)
+{
+	void *ret = calloc(nmemb, size);
+
+	if (!ret) {
+		fprintf(stderr, "Allocation failed.\n");
+		abort();
+	}
+
+	return ret;
+}
+
+void *
+ascigram_realloc(void *ptr, size_t size)
+{
+	void *ret = realloc(ptr, size);
+
+	if (!ret) {
+		fprintf(stderr, "Allocation failed.\n");
+		abort();
+	}
+
+	return ret;
+}
+
+void
+ascigram_buffer_init(
+	ascigram_buffer *buf,
+	size_t unit,
+	ascigram_realloc_callback data_realloc,
+	ascigram_free_callback data_free,
+	ascigram_free_callback buffer_free)
+{
+	assert(buf);
+
+	buf->data = NULL;
+	buf->size = buf->asize = 0;
+	buf->unit = unit;
+	buf->data_realloc = data_realloc;
+	buf->data_free = data_free;
+	buf->buffer_free = buffer_free;
+}
+
+void
+ascigram_buffer_uninit(ascigram_buffer *buf)
+{
+	assert(buf && buf->unit);
+	buf->data_free(buf->data);
+}
+
+ascigram_buffer *
+ascigram_buffer_new(size_t unit)
+{
+	ascigram_buffer *ret = ascigram_malloc(sizeof (ascigram_buffer));
+	ascigram_buffer_init(ret, unit, ascigram_realloc, free, free);
+	return ret;
+}
+
+void
+ascigram_buffer_free(ascigram_buffer *buf)
+{
+	if (!buf) return;
+	assert(buf && buf->unit);
+
+	buf->data_free(buf->data);
+
+	if (buf->buffer_free)
+		buf->buffer_free(buf);
+}
+
+void
+ascigram_buffer_reset(ascigram_buffer *buf)
+{
+	assert(buf && buf->unit);
+
+	buf->data_free(buf->data);
+	buf->data = NULL;
+	buf->size = buf->asize = 0;
+}
+
+void
+ascigram_buffer_grow(ascigram_buffer *buf, size_t neosz)
+{
+	size_t neoasz;
+	assert(buf && buf->unit);
+
+	if (buf->asize >= neosz)
+		return;
+
+	neoasz = buf->asize + buf->unit;
+	while (neoasz < neosz)
+		neoasz += buf->unit;
+
+	buf->data = buf->data_realloc(buf->data, neoasz);
+	buf->asize = neoasz;
+}
+
+
+void
+ascigram_buffer_put(ascigram_buffer *buf, const uint8_t *data, size_t size)
+{
+	assert(buf && buf->unit);
+
+	if (buf->size + size > buf->asize)
+		ascigram_buffer_grow(buf, buf->size + size);
+
+	memcpy(buf->data + buf->size, data, size);
+	buf->size += size;
+}
+
+void
+ascigram_buffer_puts(ascigram_buffer *buf, const char *str)
+{
+	ascigram_buffer_put(buf, (const uint8_t *)str, strlen(str));
+}
+
+void
+ascigram_buffer_putc(ascigram_buffer *buf, uint8_t c)
+{
+	assert(buf && buf->unit);
+
+	if (buf->size >= buf->asize)
+		ascigram_buffer_grow(buf, buf->size + 1);
+
+	buf->data[buf->size] = c;
+	buf->size += 1;
+}
+
+int
+ascigram_buffer_putf(ascigram_buffer *buf, FILE *file)
+{
+	assert(buf && buf->unit);
+
+	while (!(feof(file) || ferror(file))) {
+		ascigram_buffer_grow(buf, buf->size + buf->unit);
+		buf->size += fread(buf->data + buf->size, 1, buf->unit, file);
+	}
+
+	return ferror(file);
+}
+
+void
+ascigram_buffer_set(ascigram_buffer *buf, const uint8_t *data, size_t size)
+{
+	assert(buf && buf->unit);
+
+	if (size > buf->asize)
+		ascigram_buffer_grow(buf, size);
+
+	memcpy(buf->data, data, size);
+	buf->size = size;
+}
+
+void
+ascigram_buffer_sets(ascigram_buffer *buf, const char *str)
+{
+	ascigram_buffer_set(buf, (const uint8_t *)str, strlen(str));
+}
+
+int
+ascigram_buffer_eq(const ascigram_buffer *buf, const uint8_t *data, size_t size)
+{
+	if (buf->size != size) return 0;
+	return memcmp(buf->data, data, size) == 0;
+}
+
+int
+ascigram_buffer_eqs(const ascigram_buffer *buf, const char *str)
+{
+	return ascigram_buffer_eq(buf, (const uint8_t *)str, strlen(str));
+}
+
+int
+ascigram_buffer_prefix(const ascigram_buffer *buf, const char *prefix)
+{
+	size_t i;
+
+	for (i = 0; i < buf->size; ++i) {
+		if (prefix[i] == 0)
+			return 0;
+
+		if (buf->data[i] != prefix[i])
+			return buf->data[i] - prefix[i];
+	}
+
+	return 0;
+}
+
+void
+ascigram_buffer_slurp(ascigram_buffer *buf, size_t size)
+{
+	assert(buf && buf->unit);
+
+	if (size >= buf->size) {
+		buf->size = 0;
+		return;
+	}
+
+	buf->size -= size;
+	memmove(buf->data, buf->data + size, buf->size);
+}
+
+const char *
+ascigram_buffer_cstr(ascigram_buffer *buf)
+{
+	assert(buf && buf->unit);
+
+	if (buf->size < buf->asize && buf->data[buf->size] == 0)
+		return (char *)buf->data;
+
+	ascigram_buffer_grow(buf, buf->size + 1);
+	buf->data[buf->size] = 0;
+
+	return (char *)buf->data;
+}
+
+void
+ascigram_buffer_printf(ascigram_buffer *buf, const char *fmt, ...)
+{
+	va_list ap;
+	int n;
+
+	assert(buf && buf->unit);
+
+	if (buf->size >= buf->asize)
+		ascigram_buffer_grow(buf, buf->size + 1);
+
+	va_start(ap, fmt);
+	n = vsnprintf((char *)buf->data + buf->size, buf->asize - buf->size, fmt, ap);
+	va_end(ap);
+
+	if (n < 0) {
+#ifndef _MSC_VER
+		return;
+#else
+		va_start(ap, fmt);
+		n = _vscprintf(fmt, ap);
+		va_end(ap);
+#endif
+	}
+
+	if ((size_t)n >= buf->asize - buf->size) {
+		ascigram_buffer_grow(buf, buf->size + n + 1);
+
+		va_start(ap, fmt);
+		n = vsnprintf((char *)buf->data + buf->size, buf->asize - buf->size, fmt, ap);
+		va_end(ap);
+	}
+
+	if (n < 0)
+		return;
+
+	buf->size += n;
+}
+
+void ascigram_buffer_put_utf8(ascigram_buffer *buf, unsigned int c) {
+	unsigned char unichar[4];
+
+	assert(buf && buf->unit);
+
+	if (c < 0x80) {
+		ascigram_buffer_putc(buf, c);
+	}
+	else if (c < 0x800) {
+		unichar[0] = 192 + (c / 64);
+		unichar[1] = 128 + (c % 64);
+		ascigram_buffer_put(buf, unichar, 2);
+	}
+	else if (c - 0xd800u < 0x800) {
+		ASCIGRAM_BUFPUTSL(buf, "\xef\xbf\xbd");
+	}
+	else if (c < 0x10000) {
+		unichar[0] = 224 + (c / 4096);
+		unichar[1] = 128 + (c / 64) % 64;
+		unichar[2] = 128 + (c % 64);
+		ascigram_buffer_put(buf, unichar, 3);
+	}
+	else if (c < 0x110000) {
+		unichar[0] = 240 + (c / 262144);
+		unichar[1] = 128 + (c / 4096) % 64;
+		unichar[2] = 128 + (c / 64) % 64;
+		unichar[3] = 128 + (c % 64);
+		ascigram_buffer_put(buf, unichar, 4);
+	}
+	else {
+		ASCIGRAM_BUFPUTSL(buf, "\xef\xbf\xbd");
+	}
+}
---
+++ src/buffer.h
@@ -0,0 +1,137 @@
+/* buffer.h */
+#ifndef _ASCIGRAM_BUFFER_H
+#define _ASCIGRAM_BUFFER_H
+
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_MSC_VER)
+#define __attribute__(x)
+#define inline __inline
+#define __builtin_expect(x,n) x
+#endif
+
+/*********
+ * TYPES *
+ *********/
+
+typedef void *(*ascigram_realloc_callback)(void *, size_t);
+typedef void (*ascigram_free_callback)(void *);
+
+struct ascigram_buffer {
+	uint8_t *data;	/* actual character data */
+	size_t size;	/* size of the string */
+	size_t asize;	/* allocated size (0 = volatile buffer) */
+	size_t unit;	/* reallocation unit size (0 = read-only buffer) */
+
+	ascigram_realloc_callback data_realloc;
+	ascigram_free_callback data_free;
+	ascigram_free_callback buffer_free;
+};
+
+typedef struct ascigram_buffer ascigram_buffer;
+
+
+/*************
+ * FUNCTIONS *
+ *************/
+
+/* allocation wrappers */
+void *ascigram_malloc(size_t size) __attribute__ ((malloc));
+void *ascigram_calloc(size_t nmemb, size_t size) __attribute__ ((malloc));
+void *ascigram_realloc(void *ptr, size_t size) __attribute__ ((malloc));
+
+/* ascigram_buffer_init: initialize a buffer with custom allocators */
+void ascigram_buffer_init(
+	ascigram_buffer *buffer,
+	size_t unit,
+	ascigram_realloc_callback data_realloc,
+	ascigram_free_callback data_free,
+	ascigram_free_callback buffer_free
+);
+
+/* ascigram_buffer_uninit: uninitialize an existing buffer */
+void ascigram_buffer_uninit(ascigram_buffer *buf);
+
+/* ascigram_buffer_new: allocate a new buffer */
+ascigram_buffer *ascigram_buffer_new(size_t unit) __attribute__ ((malloc));
+
+/* ascigram_buffer_free: free the buffer */
+void ascigram_buffer_free(ascigram_buffer *buf);
+
+/* ascigram_buffer_reset: free internal data of the buffer */
+void ascigram_buffer_reset(ascigram_buffer *buf);
+
+/* ascigram_buffer_grow: increase the allocated size to the given value */
+void ascigram_buffer_grow(ascigram_buffer *buf, size_t neosz);
+
+/* ascigram_buffer_put: append raw data to a buffer */
+void ascigram_buffer_put(ascigram_buffer *buf, const uint8_t *data, size_t size);
+
+/* ascigram_buffer_puts: append a NUL-terminated string to a buffer */
+void ascigram_buffer_puts(ascigram_buffer *buf, const char *str);
+
+/* ascigram_buffer_putc: append a single char to a buffer */
+void ascigram_buffer_putc(ascigram_buffer *buf, uint8_t c);
+
+/* ascigram_buffer_putf: read from a file and append to a buffer, until EOF or error */
+int ascigram_buffer_putf(ascigram_buffer *buf, FILE* file);
+
+/* ascigram_buffer_set: replace the buffer's contents with raw data */
+void ascigram_buffer_set(ascigram_buffer *buf, const uint8_t *data, size_t size);
+
+/* ascigram_buffer_sets: replace the buffer's contents with a NUL-terminated string */
+void ascigram_buffer_sets(ascigram_buffer *buf, const char *str);
+
+/* ascigram_buffer_eq: compare a buffer's data with other data for equality */
+int ascigram_buffer_eq(const ascigram_buffer *buf, const uint8_t *data, size_t size);
+
+/* ascigram_buffer_eq: compare a buffer's data with NUL-terminated string for equality */
+int ascigram_buffer_eqs(const ascigram_buffer *buf, const char *str);
+
+/* ascigram_buffer_prefix: compare the beginning of a buffer with a string */
+int ascigram_buffer_prefix(const ascigram_buffer *buf, const char *prefix);
+
+/* ascigram_buffer_slurp: remove a given number of bytes from the head of the buffer */
+void ascigram_buffer_slurp(ascigram_buffer *buf, size_t size);
+
+/* ascigram_buffer_cstr: NUL-termination of the string array (making a C-string) */
+const char *ascigram_buffer_cstr(ascigram_buffer *buf);
+
+/* ascigram_buffer_printf: formatted printing to a buffer */
+void ascigram_buffer_printf(ascigram_buffer *buf, const char *fmt, ...) __attribute__ ((format (printf, 2, 3)));
+
+/* ascigram_buffer_put_utf8: put a Unicode character encoded as UTF-8 */
+void ascigram_buffer_put_utf8(ascigram_buffer *buf, unsigned int codepoint);
+
+/* ascigram_buffer_free: free the buffer */
+void ascigram_buffer_free(ascigram_buffer *buf);
+
+
+/* ASCIGRAM_BUFPUTSL: optimized ascigram_buffer_puts of a string literal */
+#define ASCIGRAM_BUFPUTSL(output, literal) \
+	ascigram_buffer_put(output, (const uint8_t *)literal, sizeof(literal) - 1)
+
+/* ASCIGRAM_BUFSETSL: optimized ascigram_buffer_sets of a string literal */
+#define ASCIGRAM_BUFSETSL(output, literal) \
+	ascigram_buffer_set(output, (const uint8_t *)literal, sizeof(literal) - 1)
+
+/* ASCIGRAM_BUFEQSL: optimized ascigram_buffer_eqs of a string literal */
+#define ASCIGRAM_BUFEQSL(output, literal) \
+	ascigram_buffer_eq(output, (const uint8_t *)literal, sizeof(literal) - 1)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /** _ASCIGRAM_BUFFER_H **/
